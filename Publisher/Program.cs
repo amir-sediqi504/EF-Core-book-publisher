69f5141b436906d4717ecf946ed753f2dc9fafe5
amir sediqi <amir.sediqi69@gmail.com>
2023-05-03 20:21:31 +0200

    CL file

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PublisherData;
+
+using (PublisherContext context = new PublisherContext())
+{
+    context.Database.EnsureCreated();
+
+}
+
+
---
+++ PublisherData/PublisherContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PublisherDomain;
+using Microsoft.EntityFrameworkCore;
+namespace PublisherData
+{
+    public class PublisherContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get;set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=YourDatabase");
+        }
+    }
+}
